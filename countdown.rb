f0dd54f578030a77045cf64f457c5c32e381c5dc
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-12 04:24:29 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(numb)
+  while numb>0 do
+   puts "#{numb} SECOND(S)!"
+    numb-=1
+  end 
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(numb)
+ while numb>0 do
+   puts "#{numb} SECOND(S)!"
+    numb-=1
+    sleep(1)
+  end 
+"HAPPY NEW YEAR!"
 end
